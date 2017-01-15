909dd7abb2583ccb0486b37c32438f0a6e5ce8af
Jeremy <saturn5971@gmail.com>
2017-01-15 11:13:54 -0500

    Pixel setting/unsetting now works

--- src/Grid.js
+++ src/Grid.js
@@ -4,11 +4,16 @@ class Grid extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {pixels: []};
+        this.state = {pixels: [], switchState: null};
 
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 64; i++) {
             this.state.pixels.push({id: i, on: false});
         }
+
+        document.onmouseup = (e) => {
+            e.preventDefault();
+            this.setState({switchState: null});
+        };
     }
 
     render() {
@@ -16,25 +21,36 @@ class Grid extends React.Component {
                 <Pixel 
                     key={p.id}
                     on={p.on}
-                    onClick={this.setPixel.bind(this, p, !p.on)}>
+                    onMouseDown={this.mouseDown.bind(this, p)}
+                    onMouseOver={this.mouseOver.bind(this, p)}>
                 </Pixel>)
 
-        return <div className="grid">{pixels}</div>;
+        return <div className="container">
+                    <div className="grid">{pixels}</div>
+               </div>;
     }
 
-    setPixel(p, on) {
-        console.log('clicked ' + p.id);
-        p.on = on;
-        this.setState({pixels: this.state.pixels});
+    mouseDown(p, e) {
+        e.preventDefault();
+        p.on = !p.on;
+        this.setState({pixels: this.state.pixels, switchState: p.on});
+    }
+
+    mouseOver(p, e) {
+        e.preventDefault();
+        if (this.state.switchState !== null) {
+            p.on = this.state.switchState;
+            this.setState({pixels: this.state.pixels});
+        }
     }
 }
 
 class Pixel extends React.Component {
     render() {
-        console.log(this.props);
         return <div 
             className={this.props.on ? "onPixel" : "offPixel"}
-            onClick={this.props.onClick}></div>;
+            onMouseDown={this.props.onMouseDown}
+            onMouseOver={this.props.onMouseOver}></div>;
     }
 }
 
